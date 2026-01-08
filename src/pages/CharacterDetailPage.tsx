73a6a3cbbab80c2e65315cd68464bd0f3fbc8045
Mr.Codi <diwashhkafle66@gmail.com>
2026-01-08 23:47:50 +0545

    fixed issue before deployment

--- src/pages/CharacterDetailPage.tsx
+++ src/pages/CharacterDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getCharactersById } from "../services/api";
 import type { Character } from "../types/character";
